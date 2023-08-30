2926bec78dea4c405b0e4fd936b624d8dbd1230d
William Howard <Whoward2@stu.jsu.edu>
2023-08-30 09:38:08 -0500

    Added f2()

--- Class1.java
+++ Class1.java
@@ -1,12 +1,15 @@
 /**
  *
- * @author Your Name
+ * @author William Howard
  */
 
 public class Class1 {
 
     private void f1() {
         // v1.0
+    }
+    private void f2() {
+        //Placeholder method (v2.0)
     }
 
 }
--- Class2.java
+++ Class2.java
@@ -1,12 +1,15 @@
 /**
  *
- * @author Your Name
+ * @author William Howard
  */
 
 public class Class2 {
 
     private void f1() {
         // v1.0
+    }
+    private void f2() {
+        //Placeholder method (v2.0)
     }
 
 }
